c433d050be50345b2571fcd2de7844d0504460fc
cnpepper <liu_jianjun_1@163.com>
2018-11-19 19:41:03 +0800

    使用装饰模式设计领域层

---
+++ laravel/app/Http/Controllers/Api/Customer/CustomerQueryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Customer;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CustomerQueryController extends Controller
+{
+    //
+}
