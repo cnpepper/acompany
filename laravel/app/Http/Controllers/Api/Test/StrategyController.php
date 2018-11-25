f43c5249f4f2bac1cc00d4862143e90106e9dc2d
cnpepper <liu_jianjun_1@163.com>
2018-11-25 21:34:07 +0800

    策略模式

---
+++ laravel/app/Http/Controllers/Api/Test/StrategyController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Api\Test;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StrategyController extends Controller
+{
+    //
+
+    public function provideIntegers()
+    {
+        return [
+            [
+                [['id' => 2], ['id' => 1], ['id' => 3]],
+                ['id' => 1],
+            ],
+            [
+                [['id' => 3], ['id' => 2], ['id' => 1]],
+                ['id' => 1],
+            ],
+        ];
+    }
+
+    public function provideDates()
+    {
+        return [
+            [
+                [['date' => '2014-03-03'], ['date' => '2015-03-02'], ['date' => '2013-03-01']],
+                ['date' => '2013-03-01'],
+            ],
+            [
+                [['date' => '2014-02-03'], ['date' => '2013-02-01'], ['date' => '2015-02-02']],
+                ['date' => '2013-02-01'],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideIntegers
+     *
+     * @param array $collection
+     * @param array $expected
+     */
+    public function testIdComparator($collection, $expected)
+    {
+        $obj = new Context(new IdComparator());
+        $elements = $obj->executeStrategy($collection);
+
+        $firstElement = array_shift($elements);
+        $this->assertEquals($expected, $firstElement);
+    }
+
+    /**
+     * @dataProvider provideDates
+     *
+     * @param array $collection
+     * @param array $expected
+     */
+    public function testDateComparator($collection, $expected)
+    {
+        $obj = new Context(new DateComparator());
+        $elements = $obj->executeStrategy($collection);
+
+        $firstElement = array_shift($elements);
+        $this->assertEquals($expected, $firstElement);
+    }
+    
+}
+
+class Context
+{
+    /**
+     * @var ComparatorInterface
+     */
+    private $comparator;
+
+    public function __construct(ComparatorInterface $comparator)
+    {
+        $this->comparator = $comparator;
+    }
+
+    public function executeStrategy(array $elements) : array
+    {
+        uasort($elements, [$this->comparator, 'compare']);
+
+        return $elements;
+    }
+}
+
+interface ComparatorInterface
+{
+    /**
+     * @param mixed $a
+     * @param mixed $b
+     *
+     * @return int
+     */
+    public function compare($a, $b): int;
+}
+
+class DateComparator implements ComparatorInterface
+{
+    /**
+     * @param mixed $a
+     * @param mixed $b
+     *
+     * @return int
+     */
+    public function compare($a, $b): int
+    {
+        $aDate = new \DateTime($a['date']);
+        $bDate = new \DateTime($b['date']);
+
+        return $aDate <=> $bDate;
+    }
+}
+
+class IdComparator implements ComparatorInterface
+{
+    /**
+     * @param mixed $a
+     * @param mixed $b
+     *
+     * @return int
+     */
+    public function compare($a, $b): int
+    {
+        return $a['id'] <=> $b['id'];
+    }
+}
