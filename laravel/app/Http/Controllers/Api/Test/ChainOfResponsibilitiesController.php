cf760b6c477ce9613e95134653b7e5c6d191d399
cnpepper <liu_jianjun_1@163.com>
2018-11-27 20:39:40 +0800

    责任链

--- laravel/app/Http/Controllers/Api/Test/ChainOfResponsibilitiesController.php
+++ laravel/app/Http/Controllers/Api/Test/ChainOfResponsibilitiesController.php
@@ -8,6 +8,56 @@ use App\Http\Controllers\Controller;
 class ChainOfResponsibilitiesController extends Controller
 {
     //
+
+    /**
+     * @var Handler
+     */
+    private $chain;
+
+    /**
+     * 模拟设置缓存处理器的缓存数据。
+     */
+    protected function setUp()
+    {
+        $this->chain = new HttpInMemoryCacheHandler(
+            ['/foo/bar?index=1' => 'Hello In Memory!'],
+            new SlowDatabaseHandler()
+        );
+    }
+
+    /**
+     * 模拟从缓存中拉取数据。
+     */
+    public function testCanRequestKeyInFastStorage()
+    {
+        $uri = $this->createMock('Psr\Http\Message\UriInterface');
+        $uri->method('getPath')->willReturn('/foo/bar');
+        $uri->method('getQuery')->willReturn('index=1');
+
+        $request = $this->createMock('Psr\Http\Message\RequestInterface');
+        $request->method('getMethod')
+            ->willReturn('GET');
+        $request->method('getUri')->willReturn($uri);
+
+        $this->assertEquals('Hello In Memory!', $this->chain->handle($request));
+    }
+
+    /**
+     * 模拟从数据库中拉取数据。
+     */
+    public function testCanRequestKeyInSlowStorage()
+    {
+        $uri = $this->createMock('Psr\Http\Message\UriInterface');
+        $uri->method('getPath')->willReturn('/foo/baz');
+        $uri->method('getQuery')->willReturn('');
+
+        $request = $this->createMock('Psr\Http\Message\RequestInterface');
+        $request->method('getMethod')
+            ->willReturn('GET');
+        $request->method('getUri')->willReturn($uri);
+
+        $this->assertEquals('Hello World!', $this->chain->handle($request));
+    }
 }
 
 /**
@@ -56,4 +106,70 @@ abstract class Handler
      * 声明处理方法。
      */
     abstract protected function processing(RequestInterface $request);
+}
+
+/**
+ * 创建 http 缓存处理类。
+ */
+class HttpInMemoryCacheHandler extends Handler
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @param array $data
+     * 传入数据数组参数。
+     * @param Handler|null $successor
+     * 传入处理器类对象 $successor 。
+     */
+    public function __construct(array $data, Handler $successor = null)
+    {
+        parent::__construct($successor);
+
+        $this->data = $data;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * 传入请求类对象参数 $request 。
+     * @return string|null
+     * 
+     * 返回缓存中对应路径存储的数据。
+     */
+    protected function processing(RequestInterface $request)
+    {
+        $key = sprintf(
+            '%s?%s',
+            $request->getUri()->getPath(),
+            $request->getUri()->getQuery()
+        );
+
+        if ($request->getMethod() == 'GET' && isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+
+        return null;
+    }
+}
+
+/**
+ * 创建数据库处理器。
+ */
+class SlowDatabaseHandler extends Handler
+{
+    /**
+     * @param RequestInterface $request
+     * 传入请求类对象 $request 。
+     * 
+     * @return string|null
+     * 定义处理方法，下面应该是个数据库查询动作，但是简单化模拟，直接返回一个 'Hello World' 字符串作查询结果。
+     */
+    protected function processing(RequestInterface $request)
+    {
+        // 这是一个模拟输出， 在生产代码中你应该调用一个缓慢的 （相对于内存来说） 数据库查询结果。
+
+        return 'Hello World!';
+    }
 }
