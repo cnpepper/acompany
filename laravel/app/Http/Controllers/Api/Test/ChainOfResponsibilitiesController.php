89d28816c1322b49019bc5d8f9bc0ffa24bddc35
cnpepper <liu_jianjun_1@163.com>
2018-11-27 20:38:03 +0800

    责任链

---
+++ laravel/app/Http/Controllers/Api/Test/ChainOfResponsibilitiesController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api\Test;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ChainOfResponsibilitiesController extends Controller
+{
+    //
+}
+
+/**
+ * 创建处理器抽象类 Handler 。
+ */
+abstract class Handler
+{
+    /**
+     * @var Handler|null
+     * 定义继承处理器
+     */
+    private $successor = null;
+
+    /**
+     * 输入集成处理器对象。
+     */
+    public function __construct(Handler $handler = null)
+    {
+        $this->successor = $handler;
+    }
+
+    /**
+     * 通过使用模板方法模式这种方法可以确保每个子类不会忽略调用继
+     * 承。
+     *
+     * @param RequestInterface $request
+     * 定义处理请求方法。
+     * 
+     * @return string|null
+     */
+    final public function handle(RequestInterface $request)
+    {
+        $processed = $this->processing($request);
+
+        if ($processed === null) {
+            // 请求尚未被目前的处理器处理 => 传递到下一个处理器。
+            if ($this->successor !== null) {
+                $processed = $this->successor->handle($request);
+            }
+        }
+
+        return $processed;
+    }
+
+    /**
+     * 声明处理方法。
+     */
+    abstract protected function processing(RequestInterface $request);
+}
