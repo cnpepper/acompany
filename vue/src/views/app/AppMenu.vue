1fb0030e9e68cfc0ce29313d0f05dcbb3def065c
cnpepper <liu_jianjun_1@163.com>
2018-12-04 22:44:43 +0800

    调整

--- vue/src/views/app/AppMenu.vue
+++ vue/src/views/app/AppMenu.vue
@@ -21,34 +21,6 @@
                 <el-menu-item index="/query">合同查询</el-menu-item>
             </el-submenu>
         </el-submenu>
-        <el-submenu index="3">
-            <template slot="title">OA模块</template>
-            <el-submenu index="/job">
-                <template slot="title">工单管理</template>
-                <el-menu-item index="/check">工单审核</el-menu-item>
-                <el-menu-item index="/query">工单查询</el-menu-item>
-            </el-submenu>
-        </el-submenu>
-        <el-submenu index="4">
-            <template slot="title">财务模块</template>
-        </el-submenu>
-        <el-submenu index="5">
-            <template slot="title">统计模块</template>
-            <el-submenu index="/data">
-                <template slot="title">数据管理</template>
-                <el-menu-item index="/data/query">数据查询</el-menu-item>
-            </el-submenu>
-        </el-submenu>
-        <el-submenu index="6">
-            <template slot="title">用户设置</template>
-        </el-submenu>
-        <el-submenu index="7">
-            <template slot="title">系统设置</template>
-            <el-submenu index="7-1">
-                <template slot="title">基础设置</template>
-                <el-menu-item index="/setting/base/employee">员工设置</el-menu-item>
-            </el-submenu>
-        </el-submenu>
     </el-menu>
 </template>
 
