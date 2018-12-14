f7e1274edc54b4de398a40c46b209c2c80080a64
cnpepper <liu_jianjun_1@163.com>
2018-12-14 18:52:27 +0800

    开发客户x信息组件

--- vue/src/components/CustomerInfo.vue
+++ vue/src/components/CustomerInfo.vue
@@ -2,34 +2,34 @@
     <el-row>
         <table class="crm-customer-info">
             <tr>
-                <td class="crm-title-td">公司名称</td><td>北京掌上先机</td>
-                <td class="crm-title-td">店铺名称</td><td>北京掌上先机小店</td>
-                <td class="crm-title-td">意向度</td><td>A</td>
+                <td class="crm-title-td">公司名称</td><td>{{data.company_name}}</td>
+                <td class="crm-title-td">店铺名称</td><td>{{data.shop_name}}</td>
+                <td class="crm-title-td">意向度</td><td>{{data.customer_order}}</td>
             </tr>
             <tr>
-                <td class="crm-title-td">归属</td><td>5天23小时56分</td>
-                <td class="crm-title-td">来源</td><td>5天23小时56分</td>
-                <td class="crm-title-td">来源备注</td><td></td>
+                <td class="crm-title-td">归属</td><td>{{data.customer_attach}}</td>
+                <td class="crm-title-td">来源</td><td>{{data.customer_source}}</td>
+                <td class="crm-title-td">来源备注</td><td>{{data.customer_source_remark}}</td>
             </tr>
             <tr>
-                <td class="crm-title-td">创建人</td><td>刘建均</td>
-                <td class="crm-title-td">保护人</td><td></td>
-                <td class="crm-title-td">推荐人</td><td></td>
+                <td class="crm-title-td">创建人</td><td>{{data.creator}}</td>
+                <td class="crm-title-td">保护人</td><td>{{data.is_protect}}</td>
+                <td class="crm-title-td">推荐人</td><td>{{data.customer_source_partner}}</td>
             </tr>
             <tr>
-                <td class="crm-title-td">省市区</td><td>北京-北京市-东城区</td>
-                <td class="crm-title-td">现用软件</td><td>网店管家</td>
-                <td class="crm-title-td">备注</td><td></td>
+                <td class="crm-title-td">省市区</td><td>{{data.customer_province}}</td>
+                <td class="crm-title-td">现用软件</td><td>{{data.old_software}}</td>
+                <td class="crm-title-td">备注</td><td>{{data.remark}}</td>
             </tr>
             <tr>
-                <td class="crm-title-td">一线销售</td><td>刘建均</td>
-                <td class="crm-title-td">电话销售</td><td>刘建均</td>
-                <td class="crm-title-td">备注</td><td></td>
+                <td class="crm-title-td">一线销售</td><td>{{data.sales}}</td>
+                <td class="crm-title-td">电话销售</td><td>{{data.tel_sales}}</td>
+                <td class="crm-title-td">其他</td><td>{{data.other}}</td>
             </tr>
             <tr>
-                <td class="crm-title-td">电销强开时间</td><td>5天23小时56分</td>
-                <td class="crm-title-td">一线强开时间</td><td>5天23小时56分</td>
-                <td class="crm-title-td">最新跟进时间</td><td>2018-12-14 15:42:29</td>
+                <td class="crm-title-td">电销强开时间</td><td>{{data.company_name}}</td>
+                <td class="crm-title-td">一线强开时间</td><td>{{data.company_name}}</td>
+                <td class="crm-title-td">最新跟进时间</td><td>{{data.company_name}}</td>
             </tr>
         </table>
     </el-row>
@@ -48,6 +48,7 @@
 <style>
 
 .crm-customer-info{
+    width: '100%';
     border-collapse:collapse;
 }
 .crm-customer-info table, td, th
