6e63b5d16acf4e018d78029ef70f0627ddf9b498
cnpepper <liu_jianjun_1@163.com>
2018-12-23 18:08:00 +0800

    右键菜单

--- vue/src/views/lead/LeadQueryViews.vue
+++ vue/src/views/lead/LeadQueryViews.vue
@@ -3,10 +3,9 @@
         <el-container>
             <el-header>
                 <el-button>新建线索</el-button>
-            </el-header> 
+            </el-header>
             <el-main>
-                <el-table :data="tableData" size="mini" border 
-                    @row-contextmenu="ContextMenuHandle">
+                <el-table :data="tableData" size="mini" border @row-contextmenu="ContextMenuHandle">
                     <el-table-column prop="lead_no" label="编号" width="120">
                     </el-table-column>
                     <el-table-column prop="lead_name" label="名称" width="120" :render-header="createRenderHeader">
@@ -39,23 +38,55 @@
                 </el-pagination>
             </el-main>
         </el-container>
-
+        <v-contextmenu :theme="theme" ref="contextmenu">
+            <v-contextmenu-item>菜单1</v-contextmenu-item>
+        </v-contextmenu>
+        <div :class="['box', theme]" v-oncontext:contextmenu>
+            点击此区域
+        </div>
     </div>
 </template>
 
 <script>
+    import VContextmenu from "@/components/common/menu/ContextMenu"
+    import VContextmenuGroup from "@/components/common/menu/ContextMenuGroup"
+    import VContextmenuItem from "@/components/common/menu/ContextMenuItem"
+    import VContextSubmenu from "@/components/common/menu/ContextSubMenu"
+
     export default {
+        props: {
+            theme: String,
+        },
+        directives: {
+            oncontext: {
+                // 之所以用 inserted 而不是 bind，是需要确保 contentmenu mounted 后才进行 addRef 操作
+                inserted(el, binding, vnode) {
+                    console.log(11111)
+                    const contextmenu = vnode.context.$refs[binding.arg]
+                    contextmenu.addRef({
+                        el,
+                        vnode
+                    })
+                    contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
+                },
+            }
+        },
+        components: {
+            VContextmenu,
+            VContextmenuGroup,
+            VContextmenuItem,
+            VContextSubmenu
+        },
         methods: {
-            ContextMenuHandle(row,event){
-                console.log(1111)
-                console.log(row)
-                console.log(event)
+            ContextMenuHandle(row, event) {
+                row
+                event
             },
             createRenderHeader(h, {
                 column,
                 $index
             }) {
-                let _that = this
+                $index
                 let render = Array.apply(null, {
                     length: 1
                 }).map(function () {
@@ -79,12 +110,12 @@
                                 },
                             }, [
                                 h('input', {
-                                    style:{
-                                        width:'100%'
+                                    style: {
+                                        width: '100%'
                                     },
                                     on: {
                                         'on-change': (event) => {
-                                            console.log('输入了内容')
+                                            event
                                         }
                                     }
                                 })
@@ -133,6 +164,9 @@
 </script>
 
 <style>
+.box {
+    width: 100%;
+  }
     div.el-select.el-select--mini {
         padding: 0px;
     }
