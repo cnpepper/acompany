29965f05991f65539aefac151289032cc41553ce
cnpepper <liu_jianjun_1@163.com>
2019-01-23 22:25:03 +0800

    登录接口

---
+++ laravel/app/Http/Controllers/Api/User/LogoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LogoutController extends Controller
+{
+    //
+    public function logout(){
+        // todo 退出登录
+    }
+}
