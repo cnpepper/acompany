0dff4d4c18259616724429895184c676e9db24c2
cnpepper <liu_jianjun_1@163.com>
2019-01-30 18:47:31 +0800

    退出登录

--- laravel/app/Http/Controllers/Api/User/LogoutController.php
+++ laravel/app/Http/Controllers/Api/User/LogoutController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
     //
     public function logout(){
         // todo 退出登录
+        if(Auth::guard('api')->check()){
+            Auth::guard('api')->user()->token()->delete();
+        }
     }
 }
