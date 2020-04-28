9e82b6da83abb85d80a9e088ff956468dba5029d
cnpepper <liu_jianjun_1@163.com>
2020-04-28 17:03:10 +0800

    分开项目

--- laravel/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div>
-<passport-clients></passport-clients>
-<passport-authorized-clients></passport-authorized-clients>
-<passport-personal-access-tokens></passport-personal-access-tokens>
-</div>
-@endsection
